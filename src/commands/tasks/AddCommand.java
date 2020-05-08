1f00d931cfefd8859e105e93853eacd3eba650e5
SkarletSkay <meskarlet123@gmail.com>
2020-05-08 17:45:27 +0300

    fix uml and help

--- src/commands/tasks/AddCommand.java
+++ src/commands/tasks/AddCommand.java
@@ -3,6 +3,9 @@ package commands.tasks;
 public class AddCommand extends TaskCommand{
     @Override
     public String description() {
-        return "add new command.";
+        return "Add project/task/subtask, use -e to add existing task, use: " +
+                "\nadd <project> <name>" +
+                "\nadd <task/subtask> <projectName/parentID> <taskDescription>." +
+                "\nadd <task/subtask> -e <projectName/parentID> <taskId>.";
     }
 }
